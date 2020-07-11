e31af647757bbefcfa4d57835fd49806dd160a0f
TamNguyenThi1998 <tamtitxend@gmail.com>
2020-07-11 14:44:03 +0700

    Push code

---
+++ app/src/main/java/ducthuan/com/chatapp/Activity/SendNotify.java
@@ -0,0 +1,18 @@
+package ducthuan.com.chatapp.Activity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import ducthuan.com.chatapp.R;
+
+public class SendNotify extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+
+    }
+}
